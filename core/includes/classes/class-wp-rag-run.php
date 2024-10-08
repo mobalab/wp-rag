fb406c686fcbf7f29bfcdf80e8f50e9375814c6c
KASHIMA Kazuo <k4200@kazu.tv>
2024-10-08 14:38:02 +0800

    Add files generated by Pluginplate

---
+++ core/includes/classes/class-wp-rag-run.php
@@ -0,0 +1,239 @@
+<?php
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * HELPER COMMENT START
+ * 
+ * This class is used to bring your plugin to life. 
+ * All the other registered classed bring features which are
+ * controlled and managed by this class.
+ * 
+ * Within the add_hooks() function, you can register all of 
+ * your WordPress related actions and filters as followed:
+ * 
+ * add_action( 'my_action_hook_to_call', array( $this, 'the_action_hook_callback', 10, 1 ) );
+ * or
+ * add_filter( 'my_filter_hook_to_call', array( $this, 'the_filter_hook_callback', 10, 1 ) );
+ * or
+ * add_shortcode( 'my_shortcode_tag', array( $this, 'the_shortcode_callback', 10 ) );
+ * 
+ * Once added, you can create the callback function, within this class, as followed: 
+ * 
+ * public function the_action_hook_callback( $some_variable ){}
+ * or
+ * public function the_filter_hook_callback( $some_variable ){}
+ * or
+ * public function the_shortcode_callback( $attributes = array(), $content = '' ){}
+ * 
+ * 
+ * HELPER COMMENT END
+ */
+
+/**
+ * Class Wp_Rag_Run
+ *
+ * Thats where we bring the plugin to life
+ *
+ * @package		WPRAG
+ * @subpackage	Classes/Wp_Rag_Run
+ * @author		Kashima, Kazuo
+ * @since		0.0.1
+ */
+class Wp_Rag_Run{
+
+	/**
+	 * Our Wp_Rag_Run constructor 
+	 * to run the plugin logic.
+	 *
+	 * @since 0.0.1
+	 */
+	function __construct(){
+		$this->add_hooks();
+	}
+
+	/**
+	 * ######################
+	 * ###
+	 * #### WORDPRESS HOOKS
+	 * ###
+	 * ######################
+	 */
+
+	/**
+	 * Registers all WordPress and plugin related hooks
+	 *
+	 * @access	private
+	 * @since	0.0.1
+	 * @return	void
+	 */
+	private function add_hooks(){
+	
+		add_action( 'plugin_action_links_' . WPRAG_PLUGIN_BASE, array( $this, 'add_plugin_action_link' ), 20 );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_frontend_scripts_and_styles' ), 20 );
+		add_action( 'wp_ajax_nopriv_my_demo_ajax_call', array( $this, 'my_demo_ajax_call_callback' ), 20 );
+		add_action( 'wp_ajax_my_demo_ajax_call', array( $this, 'my_demo_ajax_call_callback' ), 20 );
+		add_action( 'plugins_loaded', array( $this, 'add_wp_webhooks_integrations' ), 9 );
+		add_filter( 'wpwhpro/admin/settings/menu_data', array( $this, 'add_main_settings_tabs' ), 20 );
+		add_action( 'wpwhpro/admin/settings/menu/place_content', array( $this, 'add_main_settings_content' ), 20 );
+	
+	}
+
+	/**
+	 * ######################
+	 * ###
+	 * #### WORDPRESS HOOK CALLBACKS
+	 * ###
+	 * ######################
+	 */
+
+	/**
+	* Adds action links to the plugin list table
+	*
+	* @access	public
+	* @since	0.0.1
+	*
+	* @param	array	$links An array of plugin action links.
+	*
+	* @return	array	An array of plugin action links.
+	*/
+	public function add_plugin_action_link( $links ) {
+
+		$links['our_shop'] = sprintf( '<a href="%s" target="_blank title="Documentation" style="font-weight:700;">%s</a>', 'https://github.com/k4200/wp-rag', __( 'Documentation', 'wp-rag' ) );
+
+		return $links;
+	}
+
+
+	/**
+	 * Enqueue the frontend related scripts and styles for this plugin.
+	 *
+	 * @access	public
+	 * @since	0.0.1
+	 *
+	 * @return	void
+	 */
+	public function enqueue_frontend_scripts_and_styles() {
+		wp_enqueue_style( 'wprag-frontend-styles', WPRAG_PLUGIN_URL . 'core/includes/assets/css/frontend-styles.css', array(), WPRAG_VERSION, 'all' );
+		wp_enqueue_script( 'wprag-frontend-scripts', WPRAG_PLUGIN_URL . 'core/includes/assets/js/frontend-scripts.js', array( 'jquery' ), WPRAG_VERSION, false );
+		wp_localize_script( 'wprag-frontend-scripts', 'wprag', array(
+			'demo_var'   		=> __( 'This is some demo text coming from the backend through a variable within javascript.', 'wp-rag' ),
+			'ajaxurl' 			=> admin_url( 'admin-ajax.php' ),
+			'security_nonce'	=> wp_create_nonce( "your-nonce-name" ),
+		));
+	}
+
+
+	/**
+	 * The callback function for my_demo_ajax_call
+	 *
+	 * @access	public
+	 * @since	0.0.1
+	 *
+	 * @return	void
+	 */
+	public function my_demo_ajax_call_callback() {
+		check_ajax_referer( 'your-nonce-name', 'ajax_nonce_parameter' );
+
+		$demo_data = isset( $_REQUEST['demo_data'] ) ? sanitize_text_field( $_REQUEST['demo_data'] ) : '';
+		$response = array( 'success' => false );
+
+		if ( ! empty( $demo_data ) ) {
+			$response['success'] = true;
+			$response['msg'] = __( 'The value was successfully filled.', 'wp-rag' );
+		} else {
+			$response['msg'] = __( 'The sent value was empty.', 'wp-rag' );
+		}
+
+		if( $response['success'] ){
+			wp_send_json_success( $response );
+		} else {
+			wp_send_json_error( $response );
+		}
+
+		die();
+	}
+
+	/**
+	 * ####################
+	 * ### WP Webhooks 
+	 * ####################
+	 */
+
+	/*
+	 * Register dynamically all integrations
+	 * The integrations are available within core/includes/integrations.
+	 * A new folder is considered a new integration.
+	 *
+	 * @access	public
+	 * @since	0.0.1
+	 *
+	 * @return	void
+	 */
+	public function add_wp_webhooks_integrations(){
+
+		// Abort if WP Webhooks is not active
+		if( ! function_exists('WPWHPRO') ){
+			return;
+		}
+
+		$custom_integrations = array();
+		$folder = WPRAG_PLUGIN_DIR . 'core' . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'integrations';
+
+		try {
+			$custom_integrations = WPWHPRO()->helpers->get_folders( $folder );
+		} catch ( Exception $e ) {
+			WPWHPRO()->helpers->log_issue( $e->getTraceAsString() );
+		}
+
+		if( ! empty( $custom_integrations ) ){
+			foreach( $custom_integrations as $integration ){
+				$file_path = $folder . DIRECTORY_SEPARATOR . $integration . DIRECTORY_SEPARATOR . $integration . '.php';
+				WPWHPRO()->integrations->register_integration( array(
+					'slug' => $integration,
+					'path' => $file_path,
+				) );
+			}
+		}
+	}
+
+	/*
+	 * Add the setting tabs
+	 *
+	 * @access	public
+	 * @since	0.0.1
+	 *
+	 * @param	mixed	$tabs	All available tabs
+	 *
+	 * @return	array	$data
+	 */
+	public function add_main_settings_tabs( $tabs ){
+
+		$tabs['demo'] = WPWHPRO()->helpers->translate( 'Demo', 'admin-menu' );
+
+		return $tabs;
+
+	}
+
+	/*
+	 * Output the content of the tab
+	 *
+	 * @access	public
+	 * @since	0.0.1
+	 *
+	 * @param	mixed	$tab	The current tab
+	 *
+	 * @return	void
+	 */
+	public function add_main_settings_content( $tab ){
+
+		switch($tab){
+			case 'demo':
+				echo '<div class="wpwh-container">This is some custom text for our very own demo tab.</div>';
+				break;
+		}
+
+	}
+
+}
---
+++ wp-rag.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * WP RAG
+ *
+ * @package       WPRAG
+ * @author        Kashima, Kazuo
+ * @license       gplv3
+ * @version       0.0.1
+ *
+ * @wordpress-plugin
+ * Plugin Name:   WP RAG
+ * Plugin URI:    https://github.com/k4200/wp-rag
+ * Description:   This is some demo short description...
+ * Version:       0.0.1
+ * Author:        Kashima, Kazuo
+ * Author URI:    https://github.com/k4200
+ * Text Domain:   wp-rag
+ * Domain Path:   /languages
+ * License:       GPLv3
+ * License URI:   https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WP RAG. If not, see <https://www.gnu.org/licenses/gpl-3.0.html/>.
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * HELPER COMMENT START
+ * 
+ * This file contains the main information about the plugin.
+ * It is used to register all components necessary to run the plugin.
+ * 
+ * The comment above contains all information about the plugin 
+ * that are used by WordPress to differenciate the plugin and register it properly.
+ * It also contains further PHPDocs parameter for a better documentation
+ * 
+ * The function WPRAG() is the main function that you will be able to 
+ * use throughout your plugin to extend the logic. Further information
+ * about that is available within the sub classes.
+ * 
+ * HELPER COMMENT END
+ */
+
+// Plugin name
+define( 'WPRAG_NAME',			'WP RAG' );
+
+// Plugin version
+define( 'WPRAG_VERSION',		'0.0.1' );
+
+// Plugin Root File
+define( 'WPRAG_PLUGIN_FILE',	__FILE__ );
+
+// Plugin base
+define( 'WPRAG_PLUGIN_BASE',	plugin_basename( WPRAG_PLUGIN_FILE ) );
+
+// Plugin Folder Path
+define( 'WPRAG_PLUGIN_DIR',	plugin_dir_path( WPRAG_PLUGIN_FILE ) );
+
+// Plugin Folder URL
+define( 'WPRAG_PLUGIN_URL',	plugin_dir_url( WPRAG_PLUGIN_FILE ) );
+
+/**
+ * Load the main class for the core functionality
+ */
+require_once WPRAG_PLUGIN_DIR . 'core/class-wp-rag.php';
+
+/**
+ * The main function to load the only instance
+ * of our master class.
+ *
+ * @author  Kashima, Kazuo
+ * @since   0.0.1
+ * @return  object|Wp_Rag
+ */
+function WPRAG() {
+	return Wp_Rag::instance();
+}
+
+WPRAG();
