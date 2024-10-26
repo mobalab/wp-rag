a55e6d64b4ecb7e9d84ef61c58397b865c8879a5
KASHIMA Kazuo <k4200@kazu.tv>
2024-10-27 03:13:47 +0800

    Add uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,28 @@
+<?php
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
+
+define( 'WPRAG_NAME', 'WP RAG' );
+define( 'WPRAG_PLUGIN_FILE', __FILE__ );
+define( 'WPRAG_PLUGIN_BASE', plugin_basename( WPRAG_PLUGIN_FILE ) );
+define( 'WPRAG_PLUGIN_DIR', plugin_dir_path( WPRAG_PLUGIN_FILE ) );
+
+require_once WPRAG_PLUGIN_DIR . 'core/class-wp-rag.php';
+require_once WPRAG_PLUGIN_DIR . 'core/includes/classes/class-wp-rag-page-main.php';
+require_once WPRAG_PLUGIN_DIR . 'core/includes/classes/class-wp-rag-page-general-settings.php';
+require_once WPRAG_PLUGIN_DIR . 'core/includes/classes/class-wp-rag-page-ai-configuration.php';
+
+function WPRAG() {
+	return Wp_Rag::instance();
+}
+
+$option_names = array(
+	WPRAG()::OPTION_NAME_FOR_AUTH_DATA,
+	WPRAG()->pages['general-settings']::OPTION_NAME,
+	WPRAG()->pages['ai-configuration']::OPTION_NAME,
+);
+foreach ( $option_names as $option_name ) {
+	delete_option( $option_name );
+}
