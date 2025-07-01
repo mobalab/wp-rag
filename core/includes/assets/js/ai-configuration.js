6fdc5347194b13f205a2bd2d3b6462174a8afdd1
Issei.M <issei.m7@gmail.com>
2025-07-01 18:55:26 +0900

    Fix CS
    discussions:
    - https://github.com/mobalab/wp-rag/pull/44#discussion_r2174337227

--- core/includes/assets/js/ai-configuration.js
+++ core/includes/assets/js/ai-configuration.js
@@ -1,28 +1,33 @@
-(function($) {
-    'use strict';
+(function ($) {
+	'use strict';
 
-    $(document).ready(function() {
-        var $embeddingModelSelect = $('select[name="wp_rag_ai_options[embedding_model]"]');
-        var originalValue = $embeddingModelSelect.val();
+	$( document ).ready(
+		function () {
+			var $embeddingModelSelect = $( 'select[name="wp_rag_ai_options[embedding_model]"]' );
+			var originalValue         = $embeddingModelSelect.val();
 
-        // Get the warning message element that already exists in the HTML
-        var $warningMessage = $('#embedding-model-change-warning');
-        
-        // Only set up the change handler if the warning element exists
-        if ($warningMessage.length > 0) {
-            // Show/hide warning on model change
-            $embeddingModelSelect.on('change', function() {
-                if ($(this).val() !== originalValue) {
-                    $warningMessage.slideDown();
-                } else {
-                    $warningMessage.slideUp();
-                }
-            });
-        }
+			// Get the warning message element that already exists in the HTML.
+			var $warningMessage = $( '#embedding-model-change-warning' );
 
-        // Since WordPress admin forms don't typically use AJAX,
-        // we'll reset the original value on page load
-        // The warning will automatically be hidden via CSS on page load
-    });
+			// Only set up the change handler if the warning element exists.
+			if ($warningMessage.length > 0) {
+				// Show/hide warning on model change.
+				$embeddingModelSelect.on(
+					'change',
+					function () {
+						if ($( this ).val() !== originalValue) {
+							$warningMessage.slideDown();
+						} else {
+							$warningMessage.slideUp();
+						}
+					}
+				);
+			}
 
-})(jQuery);
+			// Since WordPress admin forms don't typically use AJAX,
+			// we'll reset the original value on page load.
+			// The warning will automatically be hidden via CSS on page load.
+		}
+	);
+
+})( jQuery );
